c12c290dda79c993ede284dcaba3e1628c7f907d
arundas17 <arundaskr192@gmail.com>
2025-04-21 12:29:10 +0530

    directrives

--- angulardocumentation/src/app/app.component.ts
+++ angulardocumentation/src/app/app.component.ts
@@ -1,30 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { NgClass } from '@angular/common'; // 
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, 
+  imports: [RouterOutlet, RegistrationComponent, LoginComponent, NgClass,NgIf], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angulardocumentation';
-  name:string="AngularDocumentation";
-  count:number=0;
-  
-  buttonClick() {
-    console.log("Button clicked!");
-    this.name="stay focus on AngularDocumentation";
-   
-  }
-  couter(){
-    this.count++;
-  }
-  couterinifite(){
-    setInterval(() => {
-    this.couter();
-    },1000) 
-  }
- 
+ userlogined='isuserloggined';
 
+ useraction(){
+  this.userlogined='userloggouted'
+ }
 }
